228372121bad9465c01024d1d3f3fba08bbc7e82
Shreyansh Arora <144546852+ShreyanshArora@users.noreply.github.com>
2024-10-23 17:26:02 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+    {
+    id: 428,
+    fullname: "Shreyansh Arora",
+    username: "https://github.com/ShreyanshArora",
+  },
   
 ];
